8db68237ec1d369342aefdc62e698a5d14fc0b8d
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-07-23 18:06:17 +0200

    ci: Configure ci pipeline

---
+++ src/test/java/disinkt/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package disinkt.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
