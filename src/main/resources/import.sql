c4c2668442fdad0375e04bcd38215d9c65d81c2d
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-09-15 18:14:17 +0200

    ci: database setup

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,21 @@
--- -- userId -> 4 -> max
--- -- userId -> 5 -> el
--- -- userId -> 9 -> lucas
--- insert into comments (user_id, content, date_posted) values (5, 'Very cute post', 1663241348579);
--- insert into comments (user_id, content, date_posted) values (5, 'I totally love this', 1663241370613);
--- insert into comments (user_id, content, date_posted) values (9, 'I agree with El, great idea imo', 1663241412990);
--- insert into comments (user_id, content, date_posted) values (4, 'Thank you guys!!!', 1663241432116);
---
--- insert into reactions (user_id, reaction_type) values (5, 0);
--- insert into reactions (user_id, reaction_type) values (9, 0);
---
--- insert into posts (user_id, date_posted, likes, dislikes, text) values (4, 1663239692000, 2, 0, 'It would be great if we could save every stray cat and dog on this planet. And also in the upside down');
---
--- insert into posts_comments (post_id, comments_id) values (1, 1);
--- insert into posts_comments (post_id, comments_id) values (1, 2);
--- insert into posts_comments (post_id, comments_id) values (1, 3);
--- insert into posts_comments (post_id, comments_id) values (1, 4);
---
--- insert into posts_reactions (post_id, reactions_id) values (1, 1);
--- insert into posts_reactions (post_id, reactions_id) values (1, 2);
---
+-- userId -> 4 -> max
+-- userId -> 5 -> el
+-- userId -> 9 -> lucas
+insert into comments (user_id, content, date_posted) values (5, 'Very cute post', 1663241348579);
+insert into comments (user_id, content, date_posted) values (5, 'I totally love this', 1663241370613);
+insert into comments (user_id, content, date_posted) values (9, 'I agree with El, great idea imo', 1663241412990);
+insert into comments (user_id, content, date_posted) values (4, 'Thank you guys!!!', 1663241432116);
+
+insert into reactions (user_id, reaction_type) values (5, 0);
+insert into reactions (user_id, reaction_type) values (9, 0);
+
+insert into posts (user_id, date_posted, likes, dislikes, text) values (4, 1663239692000, 2, 0, 'It would be great if we could save every stray cat and dog on this planet. And also in the upside down');
+
+insert into posts_comments (post_id, comments_id) values (1, 1);
+insert into posts_comments (post_id, comments_id) values (1, 2);
+insert into posts_comments (post_id, comments_id) values (1, 3);
+insert into posts_comments (post_id, comments_id) values (1, 4);
+
+insert into posts_reactions (post_id, reactions_id) values (1, 1);
+insert into posts_reactions (post_id, reactions_id) values (1, 2);
+
