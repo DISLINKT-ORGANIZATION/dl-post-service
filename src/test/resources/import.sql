684cf8c350ed3ea7ce35b5822e3bb2af89a5097b
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-09-16 10:55:28 +0200

    test: unit tests for getting posts

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -3,8 +3,6 @@
 -- userId -> 9 -> lucas
 insert into comments (user_id, content, date_posted) values (5, 'Very cute post', 1663241348579);
 insert into comments (user_id, content, date_posted) values (5, 'I totally love this', 1663241370613);
-insert into comments (user_id, content, date_posted) values (9, 'I agree with El, great idea imo', 1663241412990);
-insert into comments (user_id, content, date_posted) values (4, 'Thank you guys!!!', 1663241432116);
 
 insert into reactions (user_id, reaction_type) values (5, 0);
 insert into reactions (user_id, reaction_type) values (9, 0);
@@ -13,8 +11,6 @@ insert into posts (user_id, date_posted, likes, dislikes, text) values (4, 16632
 
 insert into posts_comments (post_id, comments_id) values (1, 1);
 insert into posts_comments (post_id, comments_id) values (1, 2);
-insert into posts_comments (post_id, comments_id) values (1, 3);
-insert into posts_comments (post_id, comments_id) values (1, 4);
 
 insert into posts_reactions (post_id, reactions_id) values (1, 1);
 insert into posts_reactions (post_id, reactions_id) values (1, 2);
