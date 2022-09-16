7c828b97388124820c5908099f56f9433cbe1d27
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-09-16 10:57:51 +0200

    fix: code smells

--- src/test/java/dislinkt/postservice/PostServiceIntegrationTest.java
+++ src/test/java/dislinkt/postservice/PostServiceIntegrationTest.java
@@ -14,25 +14,25 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = PostServiceApplication.class)
-public class PostServiceIntegrationTest {
+class PostServiceIntegrationTest {
 
     @Autowired
     private PostService postService;
 
     @Test
-    public void retrievePost_PostsExist() {
+    void retrievePost_PostsExist() {
         List<Long> ids = Arrays.asList(4L, 5L, 9L);
         UserIds userIds = new UserIds(ids);
         List<PostDto> posts = postService.retrievePosts(userIds);
-        assertEquals(posts.size(), 1);
+        assertEquals(1, posts.size());
     }
 
     @Test
-    public void retrievePost_PostsNotExist() {
+    void retrievePost_PostsNotExist() {
         List<Long> ids = Arrays.asList(11L, 12L, 13L);
         UserIds userIds = new UserIds(ids);
         List<PostDto> posts = postService.retrievePosts(userIds);
-        assertEquals(posts.size(), 0);
+        assertEquals(0,posts.size());
     }
 
 
